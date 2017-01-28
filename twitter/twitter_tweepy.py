f6caf4fd5f389abbf98fb0cb81b00ea385bef274
Anju Eappen <anjueappen@Anjus-MacBook-Pro.local>
2017-01-28 16:06:57 -0700

    getting status data

--- twitter/twitter_tweepy.py
+++ twitter/twitter_tweepy.py
@@ -1,24 +1,50 @@
 #Import the necessary methods from tweepy library
-import tweepy
-import pprint
+import datetime
 
-#Variables that contains the user credentials to access Twitter API 
-access_token = "825428485342507008-Sv7yaJk6pkFi8CNYQFHvCA5tPNcKb5S"
-access_token_secret = "z45CtmlJoSgfgFrnqeDe41bxo0cUlpvdqzB3sQq0niIt5"
-consumer_key = "eRoCZjX92AKa7GuJBR9VE0zqu"
-consumer_secret = "oOJICZMxFS7p4ufXoIVQBMDpbz9tEWVTxGpW88WgyU8Mbndra6"
+import boto.dynamodb
+import tweepy
+
+#Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "825428485342507008-Sv7yaJk6pkFi8CNYQFHvCA5tPNcKb5S"
+ACCESS_TOKEN_SECRET = "z45CtmlJoSgfgFrnqeDe41bxo0cUlpvdqzB3sQq0niIt5"
+CONSUMER_KEY = "eRoCZjX92AKa7GuJBR9VE0zqu"
+CONSUMER_SECRET = "oOJICZMxFS7p4ufXoIVQBMDpbz9tEWVTxGpW88WgyU8Mbndra6"
+
+epoch = datetime.datetime.utcfromtimestamp(0)
+conn = boto.dynamodb.connect_to_region(
+        'us-west-2',
+        aws_access_key_id='AKIAIIMMXJREASSBQ6ZA',
+        aws_secret_access_key='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')
+
+table = conn.get_table('tweets')
 
 class TwitterStreamListener(tweepy.StreamListener):
-    def on_data(self, data):
-        pprint.pprint(data)
-        print()
-        return True
+
+    def on_status(self, status):
+
+        if status.geo != None:
+            item_data = {
+                'id': status.id,
+                'text': status.text,
+                'timestamp': (status.created_at - epoch).total_seconds() * 1000.0,
+                'coordinates': status.coordinates,
+                'hashtags': status.entities['hashtags'],
+                'location': status.coordinates
+            }
+            print(status.coordinates)
 
     def on_error(self, status):
         print(status)
 
+
+class Tweet():
+
+    def __init__(self):
+        pass
+
 if __name__ == "__main__":
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     stream = tweepy.Stream(auth, TwitterStreamListener())
-    stream.sample()
+    stream.sample(1)
+
