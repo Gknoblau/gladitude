9134fd0dfde85a8ef0c9d21f35140bf47e002c8a
Anju Eappen <anjueappen@Anjus-MacBook-Pro.local>
2017-01-29 00:58:33 -0700

    do your shit kyle, s'all good

--- gladitude_server/server.py
+++ gladitude_server/server.py
@@ -1,12 +1,23 @@
 from flask import Flask
+
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import json
+from textblob import TextBlob
+import preprocessor as p
+
 app = Flask(__name__)
 
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('gladitude')
+table = dynamodb.Table('fuck')
+print(table)
 
+
+with open('../zip2fips.json') as data_file:
+    zip2fips = json.load(data_file)
+
+fips = set(zip2fips.values())
 
 RESULT = {'ID': 825535001265655808,
           'fips': 34035,
@@ -21,7 +32,18 @@ RESULT = {'ID': 825535001265655808,
 
 @app.route("/")
 def hello():
-    return json.dumps(RESULT)
+    resp = table.scan(FilterExpression=Attr("fips").exists())
+    sorted_by_fips = {}
+    for item in resp['Items']:
+        concat = " ".join(item['tweet'])
+        testimonal = TextBlob(concat)
+        polarity = (testimonal.sentiment.polarity)
+
+        sorted_by_fips[str(item['fips'])] = polarity
+
+    return json.dumps(sorted_by_fips)
+
+
 
 if __name__ == "__main__":
     app.run()
